90e0daf039aba85b9d2fe1a50c73ae5ee2a9ffd0
Kruithne <kruithne@gmail.com>
2014-05-15 18:04:57 +0100

    Added weekly quiz/trivia bar to the front-page.

--- templates/landing.php
+++ templates/landing.php
@@ -1,6 +1,13 @@
 <div class="module module-padded">
-	<div id="weekly-quiz"></div>
-	<div id="weekly-trivia"></div>
+	<div id="weekly-quiz"><p>
+			<?php
+				if ($this->weeklyQuiz == Quiz::NONE)
+					echo 'There is no Quiz of the Week yet!';
+				else
+					echo 'Quiz of the Week: ' . $this->weeklyQuiz->getName();
+			?>
+	</p><p>Click here to vote for next weeks Quiz of the Week.</p></div>
+	<div id="weekly-trivia"><p>Last months trivia winner: Coming soon!</p><p>Click here to see scores and answer this months trivia.</p></div>
 	<div class="float-block"></div>
 </div>
 <div class="module module-padded">
