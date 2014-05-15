25c6217a6c2ad63d9a28ebe55ea10f1b45ba9484
Kruithne <kruithne@gmail.com>
2014-05-15 18:06:03 +0100

    Corrected function call getName() -> getTitle() for Quiz class.

--- templates/landing.php
+++ templates/landing.php
@@ -4,7 +4,7 @@
 				if ($this->weeklyQuiz == Quiz::NONE)
 					echo 'There is no Quiz of the Week yet!';
 				else
-					echo 'Quiz of the Week: ' . $this->weeklyQuiz->getName();
+					echo 'Quiz of the Week: ' . $this->weeklyQuiz->getTitle();
 			?>
 	</p><p>Click here to vote for next weeks Quiz of the Week.</p></div>
 	<div id="weekly-trivia"><p>Last months trivia winner: Coming soon!</p><p>Click here to see scores and answer this months trivia.</p></div>
