626045bdc4e5146bf12e33e51ff2c5b862d772c5
Kruithne <kruithne@gmail.com>
2014-05-17 11:49:57 +0100

    Added checklist to the landing page and changed voting icon to a star.

--- templates/landing.php
+++ templates/landing.php
@@ -13,4 +13,12 @@
 <div class="module module-padded">
 	<p class="center">Welcome to Fundraising Quizzes, a website designed around a 'no-asking for answers' policy for quizzers to discuss quiz related topics in a light hearted, friendly manner.</p>
 	<p class="center">The feature-rich website offers a wide array of features, including a quiz page for charities to advertise upcoming quizzes, a listing of answers for past quizzes and a community driven forum.</p>
+	<div id="checklist-container">
+		<ul id="checklist">
+			<li>Absolutely no answers asked for on-going quizzes.</li>
+			<li>Listing is free for all fund-raising/charity quizzes.</li>
+			<li>Friendly community based around quiz solving.</li>
+			<li>100% free membership with no adverts/pop-ups.</li>
+		</ul>
+	</div>
 </div>
