b35443bac82da0ec5011def23385266e08ee5607
Kruithne <kruithne@gmail.com>
2014-05-20 15:25:25 +0100

    Update link parsing to support https protocol.

--- static/scripts/link_parsing.js
+++ static/scripts/link_parsing.js
@@ -6,7 +6,7 @@ $(function()
 			$('.linkable').each(function()
 			{
 				var e = $(this);
-				e.html(e.html().replace(/((http:\/\/|(www\.))+(\S+))/, '<a href="http://$3$4">$1</a>'));
+				e.html(e.html().replace(/((http(s|):\/\/|(www\.))+(\S+))/, '<a href="http$3://$4$5">$1</a>'));
 			})
 		}
 	};
