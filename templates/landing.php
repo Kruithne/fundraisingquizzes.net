8485428c733dfc3255d9c50e382d712862c4c4a6
Kruithne <kruithne@gmail.com>
2014-05-20 18:32:59 +0100

    Fixed casting issue.

--- templates/landing.php
+++ templates/landing.php
@@ -1,10 +1,10 @@
 <div class="module module-padded">
 	<div id="weekly-quiz"><p>
 			<?php
-				if ($this->weeklyQuiz == Quiz::NONE)
-					echo 'There is no Quiz of the Week yet!';
-				else
+				if ($this->weeklyQuiz instanceof Quiz)
 					echo 'Quiz of the Week: ' . $this->weeklyQuiz->getTitle();
+				else
+					echo 'There is no Quiz of the Week yet!';
 			?>
 	</p><p>Click here to vote for next weeks Quiz of the Week.</p></div>
 	<div id="weekly-trivia"><p>Last months trivia winner: Coming soon!</p><p>Click here to see scores and answer this months trivia.</p></div>
