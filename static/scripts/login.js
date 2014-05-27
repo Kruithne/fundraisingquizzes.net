4cd547485d2bf4636dd222552d5259605ed38c3d
Kruithne <kruithne@gmail.com>
2014-05-27 16:02:59 +0100

    Form styling added, updated login widget to be more interactive. Still WIP.

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -6,15 +6,29 @@ $(function()
 			this.element = $('#account-status');
 			this.loggedIn = loggedIn == undefined ? false : loggedIn;
 
-			if (this.loggedIn)
-				this.setContent('You are logged in as NOBODY!');
-			else
-				this.setContent('You are currently not logged in: login or register.');
+			this.resetLoginForm();
+
+			var d = document, c = 'click';
+			$(d).on(c, '#login-button', handler.loginButtonClick);
+			$(d).on(c, '#login-cancel', handler.resetLoginForm);
 		},
 
-		setContent: function(content)
+		loginButtonClick: function()
 		{
-			this.element.html(content);
+			var element = handler.element;
+			element.empty();
+			$('<input type="text" placeholder="Username..." id="username-field" class="input-text"/>').appendTo(element);
+			$('<input type="password" placeholder="Password..." id="password-field" class="input-text"/>').appendTo(element);
+			$('<input type="button" id="login-submit" value="Login" class="input-button"/>').appendTo(element);
+			$('<input type="button" id="login-cancel" value="Cancel" class="input-button"/>').appendTo(element);
+		},
+
+		resetLoginForm: function()
+		{
+			if (handler.loggedIn)
+				handler.element.html('You are logged in as NOBODY!');
+			else
+				handler.element.html('You are currently not logged in: <a id="login-button">login</a> or <a>register</a>.');
 		}
 	};
 
