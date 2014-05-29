89741319051506a022039f1a44310297495f5920
Kruithne <kruithne@gmail.com>
2014-05-29 16:48:39 +0100

    Call the class loader statically before initiating the system to allow objects to correctly unserizalize from the session.

--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -2,13 +2,13 @@
 	require_once('constants.php');
 	require_once('../lib/KrameWork/KrameWork/KrameSystem.php');
 
-	$system = new KrameSystem();
+	KW_ClassLoader::addClassPath('../modules');
+	KW_ClassLoader::addClassPath('../engine');
+	KW_ClassLoader::addClassPath('../engine/objects');
+	KW_ClassLoader::addClassPath('../engine/handlers');
+	KW_ClassLoader::addClassPath('../engine/interfaces');
+	KW_ClassLoader::addClassPath('../engine/listeners');
 
+	$system = new KrameSystem();
 	$system->getErrorHandler()->addEmailOutputRecipient('kruithne+fquizzes@gmail.com');
-	$system->addAutoLoadPath('../modules');
-	$system->addAutoLoadPath('../engine');
-	$system->addAutoLoadPath('../engine/objects');
-	$system->addAutoLoadPath('../engine/handlers');
-	$system->addAutoLoadPath('../engine/interfaces');
-	$system->addAutoLoadPath('../engine/listeners');
 ?>
