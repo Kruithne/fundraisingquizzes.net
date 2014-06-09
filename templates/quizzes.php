f0c6212bff7de25bdddc80ff09a720bf2d38ad28
Kruithne <kruithne@gmail.com>
2014-06-09 21:32:29 +0100

    Add: Quiz submission button, design only, no function.

--- templates/quizzes.php
+++ templates/quizzes.php
@@ -1,6 +1,9 @@
 <div class="module module-padded">
 	<p class="center">Below you will find a list of the quizzes available on this site along with a description and the charity the quiz is in aid of as well as various options for bookmarking and voting.</p>
 </div>
+<div class="module module-padded" id="submit-button">
+	Want to add your own quiz to this list? Click here to submit one!
+</div>
 <?php
 	if ($this->quizzes == null || !count($this->quizzes))
 	{
