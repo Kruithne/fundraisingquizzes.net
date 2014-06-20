592b1852b24fc074db52ede91a9c3c4271284109
Kruithne <kruithne@gmail.com>
2014-06-20 10:52:18 +0100

    Change: Shorten long URL links.

--- static/scripts/link_parsing.js
+++ static/scripts/link_parsing.js
@@ -7,6 +7,14 @@ $(function()
 				parseLinks: function()
 				{
 					this.html(this.html().replace(/(((?:http(s)?:\/\/)|(www\.))(\S+))/, '<a href="http$3://$4$5">$1</a>'));
+					this.find('a').each(function()
+					{
+						var t = $(this),
+							text = t.text();
+
+						if (text.length > 30)
+							t.text(text.substr(0, 29) + '...');
+					});
 				}
 			});
 			$('.linkable').each(function()
