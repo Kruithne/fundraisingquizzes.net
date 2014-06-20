8311f23d0219048e988f544c6c50aa83f03df4c6
Kruithne <kruithne@gmail.com>
2014-06-20 11:00:31 +0100

    Revert: Remove link shrinking as it messes with field grabbing.

--- static/scripts/link_parsing.js
+++ static/scripts/link_parsing.js
@@ -7,14 +7,6 @@ $(function()
 				parseLinks: function()
 				{
 					this.html(this.html().replace(/(((?:http(s)?:\/\/)|(www\.))(\S+))/, '<a target="_blank" href="http$3://$4$5">$1</a>'));
-					this.find('a').each(function()
-					{
-						var t = $(this),
-							text = t.text();
-
-						if (text.length > 30)
-							t.text(text.substr(0, 29) + '...');
-					});
 				}
 			});
 			$('.linkable').each(function()
