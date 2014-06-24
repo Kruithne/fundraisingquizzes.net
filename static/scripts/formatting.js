ea872df0a5f872d17750b226721ee4f36963bf4c
Kruithne <kruithne@gmail.com>
2014-06-24 21:51:32 +0100

    Add: BB code formatting.

--- static/scripts/formatting.js
+++ static/scripts/formatting.js
@@ -6,6 +6,7 @@ $(function()
 			var text = this.text();
 			text = text.replace(/\n{2,}/g, '</p><p class="break">');
 			text = text.replace(/\n/g, '<br/>');
+			text = text.replace(/\[(\/)?([bui])\]/g, '<$1$2>');
 
 			this.html('<p>' + text + '</p>');
 			return this;
@@ -17,6 +18,7 @@ $(function()
 			html = html.replace(/^<p>|<\/p>$/g, '');
 			html = html.replace(/<\/p><p class="break">/g, "\n\n");
 			html = html.replace(/<br(\/)?>/g, "\n");
+			html = html.replace(/<(\/)?([bui])>/g, '[$1$2]');
 
 			return this.html(html);
 		}
