d5236da566e673aa27df952c22cff57a4762101c
Kruithne <kruithne@gmail.com>
2014-09-07 20:56:59 +0100

    Set the thread for the current month, rather than the last month.

--- www/monthly.php
+++ www/monthly.php
@@ -10,7 +10,7 @@
 	$old_thread->setSticky(false);
 
 	// Create a new monthly thread.
-	$thread = ForumTopic::create('Monthly Winners: ' . date('F Y', strtotime("last month")), "It's the time of the month again for the monthly winners thread! Here you can post and discuss about yours and other members quiz or competition winnings! Did you win something or earn a score you're proud of and have been dying to tell everyone about it? Now is your chance!", 1, ForumTopic::TYPE_NORMAL);
+	$thread = ForumTopic::create('Monthly Winners: ' . date('F Y'), "It's the time of the month again for the monthly winners thread! Here you can post and discuss about yours and other members quiz or competition winnings! Did you win something or earn a score you're proud of and have been dying to tell everyone about it? Now is your chance!", 1, ForumTopic::TYPE_NORMAL);
 	$thread->setSticky(true);
 
 	Settings::set('monthlyWinners', $thread->getId());
