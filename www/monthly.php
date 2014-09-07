b792b01e085ffda11908b9f77cdb46d2a9e387a6
Kruithne <kruithne@gmail.com>
2014-09-07 20:27:52 +0100

    Set the monthly winners thread as a sticky and unflag the previous months.

--- www/monthly.php
+++ www/monthly.php
@@ -5,6 +5,13 @@
 	if (REST::Get('key') !== MONTHLY_KEY)
 		die();
 
+	// Get the old thread and flag it as no longer sticky.
+	$old_thread = ForumTopic::get(Settings::get('monthlyWinners'));
+	$old_thread->setSticky(false);
+
 	// Create a new monthly thread.
-	ForumTopic::create('Monthly Winners: ' . date('F Y', strtotime("last month")), "It's the time of the month again for the monthly winners thread! Here you can post and discuss about yours and other members quiz or competition winnings! Did you win something or earn a score you're proud of and have been dying to tell everyone about it? Now is your chance!", 1, ForumTopic::TYPE_NORMAL)
+	$thread = ForumTopic::create('Monthly Winners: ' . date('F Y', strtotime("last month")), "It's the time of the month again for the monthly winners thread! Here you can post and discuss about yours and other members quiz or competition winnings! Did you win something or earn a score you're proud of and have been dying to tell everyone about it? Now is your chance!", 1, ForumTopic::TYPE_NORMAL);
+	$thread->setSticky(true);
+
+	Settings::set('monthlyWinners', $thread->getId());
 ?>
